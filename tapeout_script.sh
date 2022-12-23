e5e55496c64c3715ade5f7eec237fa739b36e01d
Anirudh Sivaraman <sk.anirudh@gmail.com>
2022-12-23 13:27:06 -0500

    Simplify tapeout script.

--- tapeout_script.sh
+++ tapeout_script.sh
@@ -4,23 +4,5 @@ mkdir dependencies
 export OPENLANE_ROOT=$(pwd)/dependencies/openlane_src
 export PDK_ROOT=$(pwd)/dependencies/pdks
 export PDK=sky130B
-make setup
-
-# Synthesis and PnR using OpenLane
-make user_project_wrapper
-
-# Simulation
-make simenv
-make verify-all-rtl
-make verify-all-gl
-
-# Parasitic extraction
-make extract-parasitics
-make create-spef-mapping
-
-# Static timing analysis
-make caravel-sta
-
-# Last steps before tapeout
 make precheck
 make run-precheck
