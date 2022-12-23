6d2f3b8035931b5d380d2dd30d7ee8fed46e3e83
Anirudh Sivaraman <sk.anirudh@gmail.com>
2022-12-23 13:49:10 -0500

    This should be sky130A.
    Not sure why it says sky130B here:  caravel_user_project/docs/source/quickstart.rst

--- tapeout_script.sh
+++ tapeout_script.sh
@@ -3,7 +3,7 @@
 mkdir dependencies
 export OPENLANE_ROOT=$(pwd)/dependencies/openlane_src
 export PDK_ROOT=$(pwd)/dependencies/pdks
-export PDK=sky130B
+export PDK=sky130A
 make setup
 make precheck
 make run-precheck
